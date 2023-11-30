e6e06e3c90545c3ef699f275a85ff4bcd0e16173
ReneU88 <138782314+ReneU88@users.noreply.github.com>
2023-11-30 16:50:46 +0100

    Add files via upload

---
+++ test-scripts/fn1_unzip.ps1
@@ -0,0 +1,19 @@
+﻿$inFolder = "Z:\DataETL\FileCont1-mpd\test\0_in"
+$outFolder = "Z:\DataETL\FileCont1-mpd\test\1_unzip"
+
+#=== get ZIP folder content + extract to next dir
+$zipFiles = Get-ChildItem -Path $inFolder -Recurse -File | Where-Object { $_.Extension -eq ".zip" } 
+foreach($zipFile in $zipFiles) 
+{ 
+    Expand-Archive -Path $zipFile.FullName -DestinationPath $outFolder
+}
+
+$fileExtensions = @('.csv', '.xlsx', '.tsv', '.txt')
+$files = Get-ChildItem -Path $inFolder -Recurse | Where-Object { $_.Extension -in $fileExtensions }
+
+foreach($file in $files) 
+{
+    Copy-Item -Path $file.FullName -Destination $outFolder
+}
+
+Write-Host "files unzipped." -ForegroundColor Green
